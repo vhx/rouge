1a8dcfa3bea31c3112b749536a23f2e066b0417e
Scott Robertson <scott@scottusrobus.com>
2016-11-16 10:30:33 -0800

    fix plain text internal

--- lib/rouge/lexers/plain_text_internal.rb
+++ lib/rouge/lexers/plain_text_internal.rb
@@ -1,25 +1,8 @@
-# -*- coding: utf-8 -*- #
-
+require 'rouge'
 module Rouge
   module Lexers
-    class PlainText < Lexer
-      title "Plain Text"
-      desc "A boring lexer that doesn't highlight anything"
-
-      tag 'plaintextiternal'
-      aliases 'text'
-      filenames '*.txt'
-      mimetypes 'text/plain'
-
-      default_options :token => 'Text'
-
-      def token
-        @token ||= Token[option :token]
-      end
-
-      def stream_tokens(string, &b)
-        yield self.token, string
-      end
+    class Node < Rouge::Lexers::PlainText
+      tag 'plaintextinternal'
     end
   end
 end

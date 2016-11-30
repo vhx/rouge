614dcdec22dffbc6c9bb0c8ac72da7d1ad023a2e
Scott Robertson <scott@scottusrobus.com>
2016-11-30 08:34:13 -0800

    add json inteernal

--- lib/rouge/lexers/javascript_internal.rb
+++ lib/rouge/lexers/javascript_internal.rb
@@ -4,5 +4,8 @@ module Rouge
     class JavascriptInternal < Rouge::Lexers::Javascript
       tag 'jsinternal'
     end
+    class JSONInternal < Rouge::Lexers::JSON
+      tag 'jsoninternal'
+    end
   end
 end
